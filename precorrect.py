5df71cd2de2222cddb0e221f3b4f703a9a56e2d1
mtimofeev <m.timofeev@ria.ru>
2024-01-25 20:59:46 +0300

    fine-tune_maskedLM

---
+++ precorrect.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+from io import StringIO
+import random
+import re
+
+with open('datanews.json', encoding="utf-8") as f:
+    read_data = f.read()
+read_data = read_data.replace('\n][\n', ',\n')
+articles = pd.read_json(StringIO(read_data), orient='records')
+
+sentList = []
+pattern = re.compile(r"\n[\w]+,\s\d+.+РИА Новости", re.IGNORECASE)
+for ind in articles.index:
+    riamatch = pattern.findall(articles['Article'][ind])
+    if riamatch:
+        s = pattern.sub("", articles['Article'][ind])
+    else:
+        s = articles['Article'][ind]
+    sentList.append(s)
+
+datanews = pd.DataFrame({"text": [], "label": []})
+# checked data
+for s in sentList:
+    datanews.loc[len(datanews.index)] = [s, 1]
+
+with open('CorrectDN.json', 'w', encoding='utf-8') as file:
+    datanews.to_json(file, force_ascii=False)
+
