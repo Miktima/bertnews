9b5d3f7d8d3b0fadb788e3dbd938d27b670547c2
mtimofeev <m.timofeev@ria.ru>
2024-02-04 19:13:46 +0300

    test pipeline

--- test.py
+++ test.py
@@ -1,40 +1,123 @@
+import requests
+import nltk
+import xml.etree.ElementTree as ET
+from html.parser import HTMLParser
+import urllib3
 import re
-import torch
-from transformers import AutoTokenizer, AutoModel
-from transformers import pipeline
+import argparse
+from transformers import AutoTokenizer, AutoModel, pipeline, logging
+
+class getContent(HTMLParser):
+    # HTML parser 
+    def __init__(self, tag:str, attrs:list):
+        self.tag = tag # tag of text block
+        self.attrs = attrs # attributes of HTML element of text block
+        self.intag = 0 # flag for parsing into specified tag
+        self.result = ""
+        HTMLParser.__init__(self)
+
+    def handle_starttag(self, tag, attrs):
+        seta1 = set(self.attrs)
+        seta2 = set(attrs)
+        # Search for HTML element with tag and attribute
+        if tag == self.tag and seta1 == seta2:
+            self.intag = 1
+    
+    def handle_endtag(self, tag):
+        if tag == self.tag and self.intag == 1:
+            self.intag = 0
+
+    def handle_data(self, data):
+        # copy text data in they are into specifuied tag - attribute pair
+        if self.intag == 1:
+            self.result += data
+
+def mask_bert_sent(text, model, tokenizer):
+    # maskin whole text and return errors if available
+    tr = 0.0001 #threshold of the error (parameter !!!)
+    unmasker = pipeline("fill-mask", model=model, tokenizer=tokenizer)
+    p = re.compile(r'[\w-]+')
+    text = '''Ранее в четверг лидеры Европейского союз единогласно согласовали выделение Украине макрофинансовой помощи в размере 50 миллиардов евро на период до 2027 года с условием ежегодной отчетности со стороны ЕК и дебатов в Евросовете. Как сообщил премьер-министр Польши Дональд Туск, главу венгерского правительства Виктора Орбана "удалось убедить".
+Позиция Будапешта оставалась главным препятствием для выделения денег киевским властям. Как выяснила газета Financial Times, лидеры стран Евросоюза могли публично объявить на саммите о неконструктивном поведении Орбана и отказать Венгрии в разморозке средств из европейских фондов.
+Как подчеркивал сам венгерский премьер, такое развитие событий привело бы к армагеддону. Он заявил, что европейцам 50 миллиардов евро нужны не меньше, чем украинцам, но предложил компромисс: не выделять всю сумму сразу на четыре года, а утверждать финансирование для Киева ежегодно и единогласно.'''
+    sentArticle = nltk.tokenize.sent_tokenize(text, language="russian")
+    listErr = []
+
+    for s in sentArticle:
+        print (s)
+        iter = p.finditer(s)
+        for match in iter:
+            if match.start() == 0:
+                masktext = "[MASK]" + text[match.end():]
+            elif match.end() == len(text):
+                masktext = text[:match.start()] + "[MASK]"
+            else:
+                masktext = text[:match.start()] + "[MASK]" + text[match.end():]
+            print (masktext)
+            res = unmasker(masktext, targets=[match.group()])
+            if res[0]['score'] < tr:
+                errorrDesc = {
+                    "word": match.group(),
+                    "start": match.start(),
+                    "end": match.end(),
+                    "prob": res[0]['score']
+                }
+                listErr.append(errorrDesc)
+    return listErr
+
+
+urllib3.disable_warnings()
+nltk.download('punkt')
+
+userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit"
+headers = {
+    'user-agent': userAgent
+    }
 
 modelpath = "../model/fine-train"
 tokenizer = AutoTokenizer.from_pretrained("DeepPavlov/rubert-base-cased")
 model = AutoModel.from_pretrained(modelpath)
-# model.cuda()  # uncomment it if you have a GPU
 
-# def embed_bert_cls(text, model, tokenizer):
-#     t = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
-#     with torch.no_grad():
-#         model_output = model(**{k: v.to(model.device) for k, v in t.items()})
-#     embeddings = model_output.last_hidden_state[:, 0, :]
-#     embeddings = torch.nn.functional.normalize(embeddings)
-#     return embeddings[0].cpu().numpy()
+logging.set_verbosity_error()
 
-# def mask_bert_sent(text, model, tokenizer):
-#     unmasker = pipeline("fill-mask", model="cointegrated/rubert-tiny2", tokenizer=tokenizer)
-#     p = re.compile(r'[\w-]+')
-#     iter = p.finditer(text)
-#     for match in iter:
-#         if match.start() == 0:
-#             masktext = "[MASK]" + text[match.end():]
-#         elif match.end() == len(text):
-#             masktext = text[:match.start()] + "[MASK]"
-#         else:
-#             masktext = text[:match.start()] + "[MASK]" + text[match.end():]
-#         print ("Masktext: ", masktext)
-#         print(unmasker(masktext, targets=[match.group()]))
-#     return
+parser = argparse.ArgumentParser()
+parser.add_argument("--xml", help="use https://ria.ru/export/rss2/archive/index.xml for parsing",\
+                    action="store_true")
+args = parser.parse_args()
 
-
-# print(embed_bert_cls('привет мир', model, tokenizer).shape)
-
-text = "После начала российской военной спецоперации на Украине западные страны усилили санкционное давление на Москву."
-# mask_bert_sent(text, "cointegrated/rubert-tiny2", tokenizer)
+if args.xml:
+    xmlUrl = "https://ria.ru/export/rss2/archive/index.xml"
+    xmlResponse = requests.get(xmlUrl, headers=headers, verify=False)
+    
+    root = ET.fromstring(xmlResponse.content)
+    
+    html_body = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1">\
+    		<meta http-equiv="X-UA-Compatible" content="IE=edge"> <title>Article errors</title> </head> <body>'
+    
+    i = 0
+    parser = getContent("div", [("class", "article__text")])
+    for item in root.iter('item'):
+        if i == 5:
+            link = item.find("link").text
+            article = ""
+            htmlResponse = requests.get(link, headers=headers, verify=False)
+            parser.feed((htmlResponse.content).decode('utf-8'))
+            print ("Link: ", link)
+            html_body += "<p>Link to the article: <a href='" + link + "'>" + link + "</a></p>"
+            print ("---------------------")
+            html_body += "<p>"
+            # print (parser.result)
+            inittext = parser.result
+            errors = mask_bert_sent(inittext, modelpath, tokenizer)
+            textwe = ""
+            if len(errors) > 0:
+                for e in errors:
+                    print (e)
+            else:
+                textwe = inittext
+            html_body += textwe
+        i += 1
+else:
+    print ("Wrong key")
 
 
